06708182d0ee11732a52282730139224f89bbdc5
lim <13183271683@163.com>
2022-08-10 09:41:42 +0800

    ^b 修改上一版本逻辑bug

--- 1_StringMatcher/StringMatcher.java
+++ 1_StringMatcher/StringMatcher.java
@@ -1,16 +1,17 @@
+
 /***
  * 字符串匹配算法
  */
 public class StringMatcher {
 
     public static void main(String[] args) {
-        String string = "hey,jude",
-               targetString = "jude",
+        String string = "l'm lim, not lee, lim!",
+               targetString = "lim",
                soutPrefix = "[" + targetString + "]在[" + string + "]中第一次出现的下标是：";
-        // jdk封装方法 indexOf
-        System.out.println(soutPrefix + getFirstMatchIndex2(string, targetString));
         // 使用一个byte一个byte匹配的算法
         System.out.println(soutPrefix + getFirstMatchIndex1(string, targetString));
+        // jdk封装方法 indexOf
+        System.out.println(soutPrefix + getFirstMatchIndex2(string, targetString));
     }
 
     /**
@@ -26,13 +27,14 @@ public class StringMatcher {
      */
     private static int getFirstMatchIndex1(String string, String targrtString){
         long start = getCurrentTimeMillis();
-        byte[] bytes = string.getBytes(),
-               bytes1 = targrtString.getBytes();
-        if(bytes1.length > bytes.length || bytes1.length == 0){
+        int result = -1;
+        if(string == null || targrtString == null){ return result; }
+        byte[] bytes = string.getBytes(), bytes1 = targrtString.getBytes();
+        if(bytes1.length > bytes.length){
             methodEnd(start);
-            return -1;
+            return result;
         }
-        return recursion(bytes, 0, bytes1, 0, start);
+        return recursion(bytes, 0, bytes1, 0, 0, start);
     }
 
     /**
@@ -44,30 +46,31 @@ public class StringMatcher {
      * @param bytes             string转化成的数组
      * @param bytesStartIndex   bytes开始遍历的下标
      * @param bytes1            targrtString转化成的数组
-     * @param byte1StartIndex   targrtString开始遍历的下标
-     * @param start             开始时间
+     * @param byte1StartIndex   bytes1开始遍历的下标
+     * @param start             开始时间,用于输出方法执行耗时
+     * @param firstTimeMatchIndex
      * @return int
      * @exception
      */
-    private static int recursion(byte[] bytes, int bytesStartIndex, byte[] bytes1, int byte1StartIndex, long start){
+    private static int recursion(byte[] bytes, int bytesStartIndex, byte[] bytes1, int byte1StartIndex, int firstTimeMatchIndex, long start){
         int result = -1;
-        if(byte1StartIndex == bytes1.length){
-            methodEnd(start);
-            return bytesStartIndex - byte1StartIndex;
-        } else if(byte1StartIndex < bytes1.length && bytesStartIndex < bytes.length){
-            if(bytes1[byte1StartIndex] == bytes[bytesStartIndex] ){
-                if(byte1StartIndex == bytes1.length){
-                    methodEnd(start);
-                    return bytesStartIndex - byte1StartIndex;
-                } else {
-                    return recursion(bytes, ++bytesStartIndex, bytes1, ++byte1StartIndex , start);
-                }
-            }else {
-                return recursion(bytes, ++bytesStartIndex, bytes1, 0 , start);
+        // 下标判断，防止下标溢出
+        if(byte1StartIndex == bytes1.length || bytesStartIndex == bytes.length){ return result; }
+        if(bytes1[byte1StartIndex] == bytes[bytesStartIndex]){
+            if(byte1StartIndex == 0) { firstTimeMatchIndex = bytesStartIndex;}
+            if(byte1StartIndex == bytes1.length - 1){
+                methodEnd(start);
+                return bytesStartIndex - byte1StartIndex;
+            }else if (bytesStartIndex == bytes.length - 1){
+                methodEnd(start);
+                return result;
             }
+            return recursion(bytes, ++bytesStartIndex, bytes1, ++byte1StartIndex , firstTimeMatchIndex, start);
+        }else if(firstTimeMatchIndex != 0){
+            return recursion(bytes, ++firstTimeMatchIndex, bytes1, 0 , firstTimeMatchIndex, start);
+        }else{
+            return recursion(bytes, ++bytesStartIndex, bytes1, 0 , firstTimeMatchIndex, start);
         }
-        methodEnd(start);
-        return result;
     }
 
     private static int getFirstMatchIndex2(String string, String targrtString){
